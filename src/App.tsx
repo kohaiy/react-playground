fd2f2271bd9da9d570841c8464357435327b9749
kohai <916309979@qq.com>
2022-02-08 16:52:18 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import '@/App.scss'
+
+const linkMap = import.meta.globEager('./urls/*.ts');
+const links = Object.keys(linkMap).map(key => linkMap[key].default);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>Welcome to <b>React Playground</b>!</p>
       </header>
+      <main>
+        <ul>{
+          links.map(({ name, url }) => <li key={name}><a href={url}>{name}</a></li>)
+        }</ul>
+      </main>
     </div>
   )
 }
